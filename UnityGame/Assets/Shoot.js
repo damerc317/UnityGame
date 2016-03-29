bc18c6bad8d5384a71deb1d7894d6cf7acd8c150
damerc317 <damerc317@gmail.com>
2016-03-28 23:32:59 -0400

    its all fucked
    i made scripts but they suck so dont even look at them.

---
+++ UnityGame/Assets/Shoot.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var theEmitter = ParticleEmitter;
+var isEmitting : boolean = true;
+ 
+function Start() 
+{
+    // finds the particle emitter on this gameObject
+    // change this to find your particle effect,
+    // or simply comment out this line, and drop your particle emitter into the Inspector
+    theEmitter = GetComponent(ParticleEmitter); // finds the particle emitter on this gameObject
+}
+
+function Update()
+{
+    if (Input.GetKeyDown(KeyCode.H))
+    {
+        if (isEmitting)
+        {
+            isEmitting = false;
+            theEmitter.emit = false;
+        }
+        else
+        {
+            isEmitting = true;
+            theEmitter.emit = true;
+        }
+    }
+}
